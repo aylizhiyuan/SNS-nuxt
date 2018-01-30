e227d4f8e43d551359c87d1e2848bb7bfc5cf8ed
lizhiyuan <148334733@qq.com>
2018-01-30 17:53:30 +0800

    完成文章页面的关注效果

---
+++ pages/p/_id.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <my-header></my-header>
+        <div class="note">
+            <!--文章区域-->
+            <div class="post">
+                <!--正文-->
+                <div class="article">
+                    <h1 class="title">月薪三千，就该去死吗？</h1>
+                    <div class="author">
+                        <nuxt-link class="avatar" to="/u/123">
+                            <img src="../../assets/img/default-avatar.jpg">
+                        </nuxt-link>
+                        <div class="info">
+                            <span class="name">测试用户</span>
+                            <a class="btn-sm" href="javascript:void(0)" :class="followObj" @click="isFollow" @mouseover="noFollow" @mouseleave="beFollow">
+                                <i class="fa" :class="iconObj" ref="icon"></i>
+                                <span ref="followWord">关注</span>
+                            </a>
+                            <div class="meta">
+                                <span>2018.01.23 16:09</span>
+                                <span>字数 2273</span>
+                                <span>阅读 882</span>
+                                <span>评论 7</span>
+                                <span>喜欢 34</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--悬浮的操作框-->
+            <div class="side-bar"></div>
+            <!--对应的推荐阅读-->
+            <div class="note-bottom"></div>
+        </div>
+    </div>
+</template>
+<script>
+    import myHeader from '~/components/myHeader'
+    export default {
+        data () {
+            return {
+                followObj:{
+                    'follow':true,
+                    'following':false
+                },
+                iconObj:{
+                    'fa-plus-square-o':true,
+                    'fa-check':false
+                }
+            }
+        },
+        components:{
+            myHeader
+        },
+        methods:{
+            isFollow:function(){
+                this.followObj.follow = !this.followObj.follow;
+                this.followObj.following = !this.followObj.following;
+                this.iconObj['fa-plus-square-o'] = !this.iconObj['fa-plus-square-o'];
+                this.iconObj['fa-check'] = !this.iconObj['fa-check'];
+                let word = this.$refs.followWord.innerHTML;
+                this.$refs.followWord.innerHTML = word == '关注' ? '已关注' : '关注';
+            },
+            noFollow:function(){
+                let word = this.$refs.followWord.innerHTML;
+                if(word == '已关注'){
+
+                    this.$refs.followWord.innerHTML = '取消关注';
+                    this.$refs.icon.className = 'fa fa-close';
+                }
+            },
+            beFollow:function(){
+                let word = this.$refs.followWord.innerHTML;
+                if(word == '取消关注'){
+                    this.$refs.followWord.innerHTML = '已关注';
+                    this.$refs.icon.className = 'fa fa-check';
+                }
+            }
+        }
+    }
+</script>
