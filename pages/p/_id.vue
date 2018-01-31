77920d73468116887d7fc974362e3d7904497891
lizhiyuan <148334733@qq.com>
2018-01-31 11:31:11 +0800

    关注功能双向绑定

--- pages/p/_id.vue
+++ pages/p/_id.vue
@@ -26,6 +26,42 @@
                             </div>
                         </div>
                     </div>
+                    <div class="show-content">
+                       <p>可能是春节快要到了，游子回家的旋律吹响，一年一度的“比头疼大赛”又要拉开大幕。比什么？比工作，比资产，比儿女，比婚姻。都市人携带着远方不为所知的辛苦，在同学相聚时，多了些虚伪；在朋友重逢时，少了些真诚。
+                       </p>
+                        <p>家乡是个让人又爱又恨的地方。有时，常看见有人自嘲：在外大老板，回乡二胖子；在外大哥大，回镇三孙子；在外大名媛，回村小豆子；在外喜当爹，回家好多爹——亲爹不倒，干爹飘飘。
+                        </p>
+                    </div>
+                    <div class="show-foot">
+                        <nuxt-link to="/nb/123" class="notebook">
+                            <i class="fa fa-book"></i>
+                            <span>我的文集</span>
+                        </nuxt-link>
+                        <div class="copyright">
+                            © 著作权归作者所有
+                        </div>
+                    </div>
+                    <div class="follow-detail">
+                        <div class="info">
+                            <nuxt-link class="avatar" to="/u/123">
+                                <img src="../../assets/img/default-avatar.jpg">
+                            </nuxt-link>
+                            <a class="btn btn-success" href="javascript:void(0)" :class="followObj" @click="isFollow" @mouseover="noFollow" @mouseleave="beFollow">
+                                <i class="fa" :class="iconObj" ref="icon2"></i>
+                                <span ref="followWord2">关注</span>
+                            </a>
+                            <nuxt-link class="title" to="/u/123">
+                                测试用户
+                            </nuxt-link>
+                            <i class="fa fa-mars"></i>
+                            <p>
+                                写了 46873 字，被 26 人关注，获得了 95 个喜欢
+                            </p>
+                        </div>
+                        <div class="signature">
+                            为城市里的不安和孤寂发声。微信公众号：得及
+                        </div>
+                    </div>
                 </div>
             </div>
             <!--悬浮的操作框-->
@@ -55,19 +91,22 @@
         },
         methods:{
             isFollow:function(){
+                let word = this.$refs.followWord.innerHTML;
+                /*这里预留关注的ajax操作*/
                 this.followObj.follow = !this.followObj.follow;
                 this.followObj.following = !this.followObj.following;
                 this.iconObj['fa-plus-square-o'] = !this.iconObj['fa-plus-square-o'];
                 this.iconObj['fa-check'] = !this.iconObj['fa-check'];
-                let word = this.$refs.followWord.innerHTML;
                 this.$refs.followWord.innerHTML = word == '关注' ? '已关注' : '关注';
+                this.$refs.followWord2.innerHTML = word == '关注' ? '已关注' : '关注';
             },
             noFollow:function(){
                 let word = this.$refs.followWord.innerHTML;
                 if(word == '已关注'){
-
                     this.$refs.followWord.innerHTML = '取消关注';
                     this.$refs.icon.className = 'fa fa-close';
+                    this.$refs.followWord2.innerHTML = '取消关注';
+                    this.$refs.icon2.className = 'fa fa-close';
                 }
             },
             beFollow:function(){
@@ -75,6 +114,8 @@
                 if(word == '取消关注'){
                     this.$refs.followWord.innerHTML = '已关注';
                     this.$refs.icon.className = 'fa fa-check';
+                    this.$refs.followWord2.innerHTML = '已关注';
+                    this.$refs.icon2.className = 'fa fa-check';
                 }
             }
         }
