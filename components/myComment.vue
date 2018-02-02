01657ac74ba6b38056ff343643041d956ff29bc6
lizhiyuan <148334733@qq.com>
2018-02-02 17:20:32 +0800

    回复列表数据模拟

--- components/myComment.vue
+++ components/myComment.vue
@@ -28,6 +28,66 @@
                 </transition>
             </form>
             <!--留言的列表-->
+            <div id="normal-comment-list" class="normal-comment-list">
+                <!--留言的排序-->
+                <div class="top-title">
+                    <span>25条评论</span>
+                    <a class="author-only" href="javascript:void(0)">
+                        只看作者
+                    </a>
+                    <div class="pull-right">
+                        <a class="active" href="javascript:void(0)">
+                            按喜欢排序
+                        </a>
+                        <a href="javascript:void(0)">
+                            按时间正序
+                        </a>
+                        <a href="javascript:void(0)">
+                            按时间倒序
+                        </a>
+                    </div>
+                </div>
+                <!--留言的正文-->
+                <div class="comment-placeholder" style="display:none">
+                    <div class="author">
+                        <div class="avatar"></div>
+                        <div class="info">
+                            <div class="name"></div>
+                            <div class="meta"></div>
+                        </div>
+                    </div>
+                    <div class="title"></div>
+                    <div class="title animated-delay"></div>
+                    <div class="tool-group">
+                        <i class="fa fa-thumbs-up"></i>
+                        <div class="zan"></div>
+                        <i class="fa fa-comment"></i>
+                        <div class="zan"></div>
+                    </div>
+                </div>
+                <div :id="'comment-'+comment.id" v-for="(comment,index) in comments" class="comment">
+                    <div class="comment-content">
+                        <div class="author">
+                            <nuxt-link class="avatar" to="/u/123">
+                                <img :src="comment.user.avatar">
+                            </nuxt-link>
+                            <div class="info">
+                                <nuxt-link class="name" to="/u/123">
+                                    {{comment.user.nick_name}}
+                                </nuxt-link>
+                                <div class="meta">
+                                    <span>
+                                        {{comment.floor}}楼·
+                                        {{comment.create_at}}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="comment-wrap"></div>
+                    </div>
+                    <div class="sub-comment-list"></div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -39,7 +99,133 @@
             return {
                 send:false,
                 showEmoji:false,
-                value:''
+                value:'',
+                comments:[
+                    {
+                        id:19935725,
+                        floor:2,
+                        liked:true,
+                        likes_count:20,
+                        note_id:23054702,
+                        user_id:6780849,
+                        user:{
+                            avatar:'/default-avatar.jpg',
+                            id:6780849,
+                            is_author:false,
+                            nick_name:'七岁就很拽',
+                            badge:null,
+
+                        },
+                        create_at:'2018-01-25T09:38:14.000+08:00',
+                        children_count:4,
+                        compiled_content:'今年25岁的我，年纪轻轻月薪就已经达到2800了，加上提成满勤再加上我天生的睿智头脑，平常帮客人拿下拖鞋点下烟得点小费可以拿到3100。觉得自己这几年过得也不容易，现在这么有钱，都不知道怎么花了，开始花钱大手大脚了，以前网吧包夜都是喝自来水，现在敢喝红茶了，还是一晚上买两瓶，甚至打电话出去叫炒河粉而且还要加个鸡蛋，我觉得现在有点迷失自我，有什么办法？希望能回到初心！',
+                        children:[
+                            {
+                                id:20116365,
+                                user_id:2604707,
+                                user:{
+                                    id:2604707,
+                                    nick_name:'Bowman_'
+                                },
+                                parent_id:19935725,                                         create_at:'2018-01-30T11:23:03.000+08:00',
+                                compiled_content:'兄die,我也和你有着一样的困惑，甚至一度迷失自我...'
+                            },
+                            {
+                                id:20122216,
+                                user_id:9933465,
+                                user:{
+                                    id:9933465,
+                                    nick_name:'美女荷官'
+                                },
+                                parent_id:19935725,                                         create_at:'2018-01-30T14:25:32.000+08:00',
+                                compiled_content:'是你李大钊飘了,还是我陈独秀拿不动刀了'
+                            },
+                            {
+                                id:20122226,
+                                user_id:9964877,
+                                user:{
+                                    id:9964877,
+                                    nick_name:'保坤文化传媒'
+                                },
+                                parent_id:19935725,                                         create_at:'2018-01-30T14:25:49.000+08:00',
+                                compiled_content:'哈哈。。'
+                            },
+                        ]
+                    },
+                    {
+                        id:19935796,
+                        floor:3,
+                        liked:false,
+                        likes_count:30,
+                        create_at: "2018-01-25T09:40:18.000+08:00",
+                        note_id:23054702,
+                        user_id:6780849,
+                        user:{
+                            avatar:'/default-avatar.jpg',
+                            id:6780849,
+                            is_author:false,
+                            nick_name:'七岁就很拽',
+                            badge:null,
+
+                        },
+                        compiled_content:'作为一名混凝土方块移动工程师，我一直以3000的月薪骄傲，甚至一度迷失自我。。。看了楼主这篇文章，我找回了初心',
+                        children_count:3,
+                        children:[
+                            {
+                                id:19949215,
+                                parent_id:19935796,
+                                user_id:5954136,
+                                user:{
+                                    id:5954136,
+                                    nickname:'与笑颜开'
+                                },
+                                create_at:'2018-01-25T16:55:40.000+08:00',
+                                compiled_content:'混凝土方块移动工程师是啥子工作，我咋没听过？我也做过混凝土这行'
+                            },
+                            {
+                                id:20062029,
+                                parent_id:19935796,
+                                user_id:8914781,
+                                user:{
+                                    id:8914781,
+                                    nickname:'向天再借5厘米'
+                                },
+                                create_at:'2018-01-28T21:06:14.000+08:00',
+                                compiled_content:'<a href="/users/88ad9c9678a6" class="maleskine-author" target="_blank" data-user-slug="88ad9c9678a6">@与笑颜开</a> 搬砖'
+                            },
+                            {
+                                id:20122231,
+                                parent_id:19935796,
+                                user_id:9964877,
+                                user:{
+                                    id:9964877,
+                                    nickname:'保坤文化传媒'
+                                },
+                                create_at:'2018-01-25T16:55:40.000+08:00',
+                                compiled_content:'好有意思。'
+                            },
+                        ]
+                    },
+                    {
+                        children: [],
+                        children_count:0,
+                        compiled_content: "楼上评论的都是大佬啊",
+                        create_at: "2018-01-29T11:26:53.000+08:00",
+                        floor:4,
+                        id:20080144,
+                        liked:false,
+                        likes_count:2,
+                        note_id: 23054702,
+                        user:
+                            {
+                                id: 3160769,
+                                nick_name: "yuebiubiu",
+                                is_author:false,
+                                avatar:'/default-avatar.jpg'
+                            },
+                        user_id:3160769
+                    }
+                ]
             }
         },
         methods:{
@@ -71,7 +257,6 @@
     }
     .note .post .comment-list {
         padding-top:20px;
-        margin-bottom:200px;/*注意,一会儿要删*/
     }
     .note .post .comment-list .new-comment {
         position:relative;
@@ -154,6 +339,7 @@
         border:1px solid #d9d9d9;
         border-radius:4px;
         box-shadow: 0 5px 25px rgba(0, 0, 0, 0.1);
+        background:#fff;
         z-index:10001;
     }
     .arrow-up:after {
@@ -169,4 +355,50 @@
         background:#fff;
         transform: translate3d(0,-50%,0) rotate(45deg);
     }
+    .note .post .comment-list .normal-comment-list {
+        margin-top:30px;
+    }
+    .note .post .comment-list .top-title {
+        padding-bottom:20px;
+        border-bottom:1px solid #f0f0f0;
+    }
+    .note .post .comment-list .top-title span {
+        font-size:17px;
+        font-weight:700;
+    }
+    .note .post .comment-list .top-title .author-only {
+        font-size:12px;
+        padding:4px 8px;
+        border:1px solid #e1e1e1;
+        border-radius:12px;
+        color:#969696!important;
+        margin-left:10px;
+    }
+    .note .post .comment-list .top-title .pull-right a {
+        margin-left:10px;
+        font-size:12px;
+        color:#969696!important;
+    }
+    .note .post .comment-list .top-title .pull-right a.active {
+        color:#2f2f2f!important;
+    }
+    .note .post .comment-list .comment {
+        padding:20px 0 30px 0;
+        border-bottom:1px solid #f0f0f0;
+    }
+    .note .post .comment-list .info {
+        display:inline-block;
+        vertical-align: middle;
+    }
+    .note .post .comment-list .info .name {
+        font-size:15px;
+    }
+    .note .post .comment-list .info .meta {
+        font-size:12px;
+        color:#969696;
+    }
+
+
+
+
 </style>
