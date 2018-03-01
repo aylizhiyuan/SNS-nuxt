d18b819bf25f3f8860c8efb79d9cb8c2d670d052
lizhiyuan <148334733@qq.com>
2018-03-01 11:11:29 +0800

    关注的文集以及喜欢

---
+++ components/users/FollowCollection.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <ul class="user-list">
+            <li>
+                <nuxt-link class="avatar-collection" to="/collection/123">
+                    <img src="~assets/img/book.png">
+                </nuxt-link>
+                <div class="info">
+                    <nuxt-link class="name" to="/collection/123">赏心何止三两枝</nuxt-link>
+                    <div class="meta">
+                        收录了20篇文章,5人关注
+                    </div>
+                </div>
+                <a class="btn btn-success follow">
+                    <i class="fa fa-plus-square-o"></i>
+                    <span>关注</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        name:"FollowCollection"
+    }
+</script>
---
+++ pages/users/_id/like.vue
@@ -0,0 +1,183 @@
+<template>
+    <div>
+        <my-header></my-header>
+        <div class="person my-container">
+            <div class="row">
+                <div class="col-8 main">
+                    <div class="main-top">
+                        <nuxt-link class="avatar" to="/u/123">
+                            <img src="~assets/img/default-avatar.jpg">
+                        </nuxt-link>
+                        <button class="off user-follow-button">
+                            <i class="fa fa-plus-square-o"></i>
+                            <span>关注</span>
+                        </button>
+                        <a class="btn btn-hollow" href="/notifications">
+                            发简信
+                        </a>
+                        <div class="title">
+                            <nuxt-link class="name" to="/u/123">
+                                我是不和么
+                            </nuxt-link>
+                            <i class="fa fa-mars"></i>
+                        </div>
+                        <div class="info">
+                            <ul>
+                                <li>
+                                    <div class="meta-block">
+                                        <nuxt-link to="/users/123/following">
+                                            <p>88</p>
+                                            关注
+                                            <i class="fa fa-angle-right"></i>
+                                        </nuxt-link>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="meta-block">
+                                        <nuxt-link to="/users/123/followers">
+                                            <p>16</p>
+                                            粉丝
+                                            <i class="fa fa-angle-right"></i>
+                                        </nuxt-link>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="meta-block">
+                                        <nuxt-link to="/u/123">
+                                            <p>32</p>
+                                            文章
+                                            <i class="fa fa-angle-right"></i>
+                                        </nuxt-link>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="meta-block">
+                                        <p>5000</p>
+                                        字数
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="meta-block">
+                                        <p>100</p>
+                                        收获喜欢
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <ul class="trigger-menu">
+                        <li :class="{active:currentTab == 'FollowCollection'}">
+                            <a href="javascript:void(0)" @click="toggleTab('FollowCollection')">
+                                关注的专题/文集/连载 6
+                            </a>
+                        </li>
+                        <li :class="{active:currentTab == 'LikeArticle'}">
+                            <a href="javascript:void(0)" @click="toggleTab('LikeArticle')">
+                                喜欢的文章 2
+                            </a>
+                        </li>
+                    </ul>
+                    <div id="list-container">
+                        <!--动态组件-->
+                        <component :is="currentTab" keep-alive></component>
+                    </div>
+                </div>
+                <div class="col-4 aside">
+                    <div class="title">个人介绍</div>
+                    <a class="function-btn"  href="javascript:void(0)">
+                        <i class="fa fa-pencil-square-o"></i> 编辑
+                    </a>
+                    <div class="description">
+                        <div class="js-intro">
+                            逃跑医生，预防医学专家，抗衰老顾问，创业者，奶爸！人生意义思考者，东方智慧践行者。非著名作家坯子。
+                        </div>
+                    </div>
+                    <ul class="list user-dynamic">
+                        <li>
+                            <nuxt-link to="/users/123/collection">
+                                <i class="fa fa-book"></i>
+                                <span>他关注的专题/文集</span>
+                            </nuxt-link>
+                        </li>
+                        <li>
+                            <nuxt-link to="/users/123/like">
+                                <i class="fa fa-heart-o"></i>
+                                <span>他喜欢的文章</span>
+                            </nuxt-link>
+                        </li>
+                    </ul>
+                    <div>
+                        <!--创建的专题-->
+                        <div class="title">
+                            他创建的专题
+                        </div>
+                        <nuxt-link to="/collection/new" target="_blank" class="function-btn new-collection-btn">
+                            <i class="fa fa-plus"></i><span>新建专题</span>
+                        </nuxt-link>
+                        <ul class="list">
+                            <li>
+                                <nuxt-link to="/collection/123" class="avatar-collection">
+                                    <img src="~assets/img/movie.jpg">
+                                </nuxt-link>
+                                <nuxt-link to="/collection/123" class="name">
+                                    朱焘源值班报告
+                                </nuxt-link>
+                            </li>
+                        </ul>
+                        <!--管理的专题-->
+                        <div class="title">
+                            他管理的专题
+                        </div>
+                        <ul class="list">
+                            <li>
+                                <nuxt-link to="/collection/123" class="avatar-collection">
+                                    <img src="~assets/img/movie.jpg">
+                                </nuxt-link>
+                                <nuxt-link to="/collection/123" class="name">
+                                    朱焘源值班报告
+                                </nuxt-link>
+                            </li>
+                        </ul>
+                        <!--创建的文集-->
+                        <div class="title">
+                            他创建的文集
+                        </div>
+                        <ul class="list">
+                            <li>
+                                <nuxt-link to="/note/123" class="avatar-collection">
+                                    <i class="fa fa-book"></i>
+                                </nuxt-link>
+                                <nuxt-link to="/note/123" class="name">
+                                    朱焘源值班报告
+                                </nuxt-link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import myHeader from '~/components/myHeader'
+    import FollowCollection from '~/components/users/FollowCollection'
+    import LikeArticle from '~/components/users/LikeArticle'
+    export default {
+        name:'',
+        data () {
+            return {
+                currentTab:'LikeArticle'
+            }
+        },
+        methods:{
+            toggleTab:function(tab){
+                this.currentTab = tab;
+            }
+        },
+        components:{
+            myHeader,
+            FollowCollection,
+            LikeArticle
+        }
+    }
+</script>
